7dd47163856493b834c6453baa9c63c89f5b6a17
Ranjith <ranjithstar256@gmail.com>
2023-07-13 11:06:32 +0530

    added new comp fn

--- app/src/main/java/com/example/memesapi/MainActivity.kt
+++ app/src/main/java/com/example/memesapi/MainActivity.kt
@@ -1,39 +1,50 @@
 package com.example.memesapi
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavHostController
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberImagePainter
+import coil.size.Scale
+import coil.transform.CircleCropTransformation
 import com.example.memesapi.ui.theme.MemesApiTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,31 +58,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    Greeting("Android")
-//                    MainScreen(
-//                        applicationContext,
-//                        memesList = mainViewModel.memesListResponse,
-//                        navController
-//                    )
-                    App(applicationContext,
-                        memesList = mainViewModel.memesListResponse,)
+                    App(memesList = mainViewModel.memesListResponse,)
                     mainViewModel.getMemesInstance()
                 }
             }
         }
     }
 }
-
-
 @Composable
-fun App(applicationContext: Context, memesList: List<memesList>) {
+fun App(memesList: List<memesList>) {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
         startDestination = "home"
     ){
         composable("home"){
-            MainScreen(applicationContext,memesList,navController = navController)
+            MainScreen(memesList)
         }
         composable("description/{lines}/{overlays}/{source}"){
             NavBackStackEntry ->
@@ -82,73 +84,112 @@ fun App(applicationContext: Context, memesList: List<memesList>) {
                     description(lines,overlays,source)
             }
         }
-//        composable("afterlogin/{mobile}"){
-//                NavBackStackEntry ->
-//            val mobile = NavBackStackEntry.arguments?.getString("mobile")
-//            if (mobile != null) {
-//                AfterLoginScreen(mobile = mobile, applicationContext = applicationContext,navController)
-//            }
-//        }
 
     }
 }
 @Composable
-fun MainScreen(
-    applicationContext: Context,
-    memesList: List<memesList>,
-    navController: NavHostController
-)
-{
-    Log.d("meme","$memesList")
-    Column(Modifier.padding(10.dp), verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Latest MEMES", fontSize = 32.sp, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
-        LazyColumn {
+fun MainScreen( memesList: List<memesList>) {
+    //Log.d("meme","$memesList")
+    Column(
+        Modifier.padding(10.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Latest MEMES",
+            fontSize = 32.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
 
-            itemsIndexed(items = memesList) {
-                    index, item ->
+        LazyColumn {
+            itemsIndexed(items = memesList) { index, item ->
                 Card(
                     Modifier
                         .padding(all = 10.dp)
-                        .fillMaxWidth()
+                        .fillMaxSize()
                         .clickable {
-//                            navController.navigate("description/${item.lines}/${item.overlays}/${item.source}")
                         }) {
                     if (memesList.isNotEmpty()) {
-                        Log.d("meme","$item")
-//                        Row(Modifier.padding(10.dp)) {
-                            Image(painter = rememberImagePainter(data = "https://api.memegen.link/images/agnes.png",
-                                builder = {
-                                    scale(coil.size.Scale.FILL)
-                            placeholder(R.drawable.placeholder)
-                                })
-                                , contentDescription = "")
-//                            Column() {
-//                                Text(text = "Name : ${item.name}",Modifier.padding(10.dp))
-//                                Text(text = "Id : ${item.id}",Modifier.padding(10.dp))
-//                                Text(text = "Overlays : ${item.overlays}",Modifier.padding(10.dp))
-//                                Text(text = "Lines : ${item.lines}",Modifier.padding(10.dp))
-//                            }
-//                        }
-
-                        Log.d("url","${item.blank}")
-
-//                        var painter = rememberImagePainter(
-//                            data = item.meme?.url,
-//                            builder = {
-//                                scale(Scale.FILL)
-//                                placeholder(R.drawable.placeholder)
-//                                transformations(CircleCropTransformation())
-//                            }
-//                        )
+                        Column(Modifier.padding(10.dp)) {
+                            MovieItem(st = "https://api.memegen.link/images/agnes.png")
+                            Button(onClick = { /*TODO*/ }) {
+                                Text(text = "Great")
+                            }
+                        }
                     }
                 }
 
             }
         }
-
-
     }
 }
+
+@Composable
+fun MovieItem(st:String)
+{
+
+
+    Card(
+        modifier = Modifier
+            .padding(8.dp, 4.dp)
+            .fillMaxSize()
+            .selectable(true, true, null,
+                onClick = {
+                })
+            .height(180.dp), shape = RoundedCornerShape(8.dp)
+    ) {
+        Surface(color = Color.White) {
+
+            Row(
+                Modifier
+                    .padding(4.dp)
+                    .fillMaxSize()
+
+            )
+            {
+                Image(
+                    painter = rememberImagePainter(
+                        data = st,
+                        builder = {
+                            scale(Scale.FILL)
+                            placeholder(R.drawable.placeholder)
+                            transformations(CircleCropTransformation())
+                        }
+                    ),
+                    contentDescription = "movie.bio",
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(0.3f)
+                )
+
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxHeight()
+                        .weight(0.8f)
+                        .background(Color.Yellow)
+                        .padding(20.dp)
+                        .selectable(true, true, null,
+                            onClick = {
+
+                            })
+                ) {
+
+                    Text(
+                        text = "c",
+                        fontWeight = FontWeight.Bold
+                    )
+
+                }
+            }
+        }
+    }
+
+}
+
 
 @Composable
 fun description(lines: Int, overlays: Int, source: String) {
@@ -161,18 +202,4 @@ fun description(lines: Int, overlays: Int, source: String) {
 
     }
 }
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MemesApiTheme {
-        Greeting("Android")
-    }
-}
